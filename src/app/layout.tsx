9be913f67a01070f7f6d6bc5c9eaed045b842ccb
Jonathan Ugalde Gonzalez <jonathan82st@gmail.com>
2025-09-19 03:55:37 +0000

    2025-09-21T03:53 este es el formato que muestra debería tener un separad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Clic-Tools",
@@ -17,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" suppressHydrationWarning>
-      <body className={`${inter.variable} font-sans antialiased`}>
+      <body>
         {children}
         <Toaster />
       </body>
