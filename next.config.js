e269d2620acc3ccdd40805092bd76d69e1b335f5
Jonathan Ugalde Gonzalez <jonathan82st@gmail.com>
2025-09-30 00:01:44 +0000

    lo ejecute de nuevo y : ./src/app/dashboard/admin/general/page.tsx Modul

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    paths: {
+      "@/*": ["./src/*"]
+    }
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: '15mb',
