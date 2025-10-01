c06a7ab02b8f7b243dd884590e12367b67c5d48b
Jonathan Ugalde Gonzalez <jonathan82st@gmail.com>
2025-10-01 22:29:57 +0000

    esta es el link que estoy usando cuando dio el error http://192.168.1.24

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    // Keeping this block if other experimental features are needed in the future,
-    // but moving serverActions out as per the new configuration standard for this issue.
-  },
   serverActions: {
     bodySizeLimit: '15mb',
-    // By not specifying `allowedOrigins`, we disable the origin check,
-    // which is the recommended approach for solving proxy-related fetch errors.
   },
   images: {
     remotePatterns: [
